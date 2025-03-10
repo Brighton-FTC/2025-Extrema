0a818b0bc3c9df85e341412f8c3d3d4da56bf8a8
Jammy <jamjamfr@proton.me>
2025-03-10 16:46:03 +0000

    Update GrabberComponent.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GrabberComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GrabberComponent.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class GrabberComponent {
@@ -11,14 +12,21 @@ public class GrabberComponent {
     private final ServoEx leftClaw;
     private final ServoEx rightClaw;
 
+    private final ServoEx grabberRotateMotor;
+
     private boolean closed = true;
 
-    public GrabberComponent(HardwareMap hardwareMap, String leftId, String rightId) {
+    public GrabberComponent(HardwareMap hardwareMap, String leftId, String rightId, String genId) {
         leftClaw = new SimpleServo(hardwareMap, leftId, 0, 360);
         rightClaw = new SimpleServo(hardwareMap, rightId, 0, 360);
+        grabberRotateMotor = new SimpleServo(hardwareMap, genId, 0, 360);
+
     }
 
+
+
     public void grab() {
+        grabberRotateMotor.rotateBy(TURN_AMOUNT);
         leftClaw.rotateBy(-TURN_AMOUNT);
         rightClaw.rotateBy(TURN_AMOUNT);
         closed = true;
@@ -26,6 +34,7 @@ public class GrabberComponent {
     }
 
     public void reset() {
+        grabberRotateMotor.rotateBy(-TURN_AMOUNT);
         leftClaw.rotateBy(TURN_AMOUNT);
         rightClaw.rotateBy(-TURN_AMOUNT);
         closed = false;
